042471a68597046d328f14c568ae4654918123ba
Amartya Jash <amartya0009@gmail.com>
2021-05-25 12:05:11 +0530

    Ifixit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Footer from '../components/Footer/Footer';
 import styles from '../styles/Home.module.scss';
 import Gravatar from 'react-gravatar';
